b8cc0bdfa366c7b539de6da2e3eaa75227f67e35
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2022-04-28 17:17:48 -0400

    fix identation

--- linear_search.py
+++ linear_search.py
@@ -1,12 +1,12 @@
 def linear_search(list, target):
-  """
-  Returns the index position of the target if found, else returns none
-  """
+    """
+    Returns the index position of the target if found, else returns none
+    """
 
-  for i in range(0, len(list)):
-    if list[i] == target:
-      return i
-  return None
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return None
 
 
 
